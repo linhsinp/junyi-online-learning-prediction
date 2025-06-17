6fde6f352c252ac709f5d3c10db8f056029cf83b
linhsinp <linhsinp@gmail.com>
2025-06-17 15:41:16 +0200

    update data ingestion task: fetch_from_gcs

--- flyte/tasks/ingest.py
+++ flyte/tasks/ingest.py
@@ -1,7 +1,22 @@
 from flytekit import task
-from scripts.gcs_utils import download_blob
+from flytekit import current_context
+import os
+from typing import Literal
+from scripts.gcs_utils import download_data_to_tmp
+
 
 @task
-def ingest_data_gcs(bucket: str, blob: str, local_path: str = "/tmp/junyi.csv") -> str:
-    download_blob(bucket, blob, local_path)
-    return local_path
+def fetch_from_gcs(prefix: Literal["raw", "experiment", "feature_store"], local_dir: str = "/tmp/data") -> str:
+    """
+    General Flyte task to download data from GCS to a specified local directory.
+
+    Args:
+        prefix (str): The folder in the GCS bucket to download from.
+        local_dir (str): Local root directory to download data into.
+
+    Returns:
+        str: Path to the downloaded data on the local filesystem.
+    """
+    print(f"Downloading GCS data from prefix '{prefix}' to local dir '{local_dir}'")
+    download_data_to_tmp(prefix=prefix, local_dir=local_dir)
+    return os.path.join(local_dir, prefix)
